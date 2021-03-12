b0ca9ea7ccbb5a47c40940160152d3587fe0cee5
Daniele <danielelai86@gmail.com>
2021-03-12 19:49:14 +0100

    Aggiunte prime funzioni per bullet

--- main.js
+++ main.js
@@ -4,26 +4,55 @@ $(function () {
   $(".next").click(
     function () {
       next();
+      nextBullet();
     }
   );
 
   $(".prev").click(
     function () {
       prev();
+      prevBullet();
     }
   );
+
+  //Pallini
+  $(".nav > i").click(function () {
+    $(".nav > i").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  function nextBullet() {
+    var pallino = $(".nav > i.active");
+    if (pallino.hasClass("last") == true) {
+      $(".nav > i.first").addClass("active");
+    }
+    pallino.removeClass("active");
+    pallino.next().addClass("active");
+  }
+
+  function prevBullet() {
+    var pallino = $(".nav > i.active");
+    if (pallino.hasClass("first") == true) {
+      $(".nav > i.last").addClass("active");
+    }
+    pallino.removeClass("active");
+    pallino.prev().addClass("active");
+  }
 
   //Keyboard event
   $(document).keydown(function(e){
     if (e.which == 37) {
       prev();
+      prevBullet();
     }
     if (e.which == 39) {
       next();
+      nextBullet();
     }
 
   });
 
+  //Funzioni
   function next() {
     var immagine = $(".images img.active");
     if ( immagine.hasClass("last") == true) {
@@ -41,5 +70,6 @@ $(function () {
     immagine.removeClass("active");
     immagine.prev().addClass("active");
   };
+
 
 });
