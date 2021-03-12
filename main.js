99feb6b6fdd1cc69ac92492154c00d492395f8e5
Daniele <danielelai86@gmail.com>
2021-03-12 17:53:40 +0100

    Aggiunti eventi keyboard

--- main.js
+++ main.js
@@ -23,7 +23,28 @@ $(function () {
     }
   );
 
-  
+  $(document).keydown(function(e){
+    if (e.which == 37) {
+      var immagine = $(".images img.active");
+      if ( immagine.hasClass("first") == true) {
+        $(".images img.last").addClass("active");
+      }
+      immagine.removeClass("active");
+      immagine.prev().addClass("active");
+    }
+    if (e.which == 39) {
+      var immagine = $(".images img.active");
+      if ( immagine.hasClass("last") == true) {
+        $(".images img.first").addClass("active");
+      }
+      immagine.removeClass("active");
+      immagine.next().addClass("active");
+    }
+
+
+
+});
+
 
 
 });
